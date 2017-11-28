854dccc4375e8792a7fa42db03a95ffe0595f509
sixblades <tutukiqnk@gmail.com>
2017-11-28 13:13:41 +0100

    ggggg

--- src/prueba_web/prueba.java
+++ src/prueba_web/prueba.java
@@ -3,7 +3,8 @@ package prueba_web;
 public class prueba  {
 	
 	 public static void main(String[] args) {
-		    System.out.println("Hola mundo.");
+		    System.out.println("Hola tijuana como estas");
+		    System.out.println("Hola wey");
 		  }
 
 }
