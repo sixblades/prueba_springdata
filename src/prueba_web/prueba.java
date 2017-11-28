6e9881a74908369958ffbbd591a0a19392ee5fc6
sixblades <tutukiqnk@gmail.com>
2017-11-28 13:14:30 +0100

    vvv

--- src/prueba_web/prueba.java
+++ src/prueba_web/prueba.java
@@ -4,7 +4,7 @@ public class prueba  {
 	
 	 public static void main(String[] args) {
 		    System.out.println("Hola tijuana como estas");
-		    System.out.println("Hola wey");
+		    System.out.println("Hola weyffff");
 		  }
 
 }
